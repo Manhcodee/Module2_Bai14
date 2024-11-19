307ff3c08c11f461ac885226aeef4f3a77a4eb37
Manhcodee <phammanh2952@gmail.com>
2024-11-19 17:04:18 +0700

    fix

--- BubbleSortByStep/src/BubbleSortByStep.java
+++ BubbleSortByStep/src/BubbleSortByStep.java
@@ -24,21 +24,21 @@ public class BubbleSortByStep {
             needNextPass = false;
             for (int i = 0; i < list.length - k; i++) {
                 if (list[i] > list[i + 1]) {
-                    /* Swap list[i] with list[i + 1] */
+
                     System.out.println("Swap " + list[i] + " with " + list[i + 1]);
                     int temp = list[i];
                     list[i] = list[i + 1];
                     list[i + 1] = temp;
 
-                    needNextPass = true; /* Next pass still needed */
+                    needNextPass = true;
                 }
             }
-            /* Array may be sorted and next pass not needed */
+
             if (needNextPass == false) {
                 System.out.println("Array may be sorted and next pass not needed");
                 break;
             }
-            /* Show the list after sort */
+            /
             System.out.print("List after the  " + k + "' sort: ");
             for (int j = 0; j < list.length; j++) {
                 System.out.print(list[j] + "\t");
@@ -46,5 +46,4 @@ public class BubbleSortByStep {
             System.out.println();
         }
     }
-
 }
